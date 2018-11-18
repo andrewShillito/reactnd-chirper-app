88b7831f72790235883bcf4f29d27d244eb4544d
Andrew Shillito <andrew.shillito88@gmail.com>
2018-11-18 19:38:58 +0000

    chore: add react-icons dependency

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -33,14 +33,4 @@ function mapStateToProps({authedUser, users, tweets}, { id }) {
     };
 }
 
-// "8xf0y6ziyjabvozdd253nd": {
-//     id: "8xf0y6ziyjabvozdd253nd",
-//     text: "Shoutout to all the speakers I know for whom English is not a first language, but can STILL explain a concept well. It's hard enough to give a good talk in your mother tongue!",
-//     author: "sarah_edo",
-//     timestamp: 1518122597860,
-//     likes: ['tylermcginnis'],
-//     replies: ['fap8sdxppna8oabnxljzcv', '3km0v4hf1ps92ajf4z2ytg'],
-//     replyingTo: null,
-//   },
-
 export default connect(mapStateToProps)(Tweet);
