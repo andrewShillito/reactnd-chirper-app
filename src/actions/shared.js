0abce35d1704d527348c94e3d5af6a0d2d2586ad
Andrew Shillito <andrew.shillito88@gmail.com>
2018-11-18 20:13:39 +0000

    feat: add loading bar, work on tweet ui

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,6 +2,7 @@ import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
 import { setAuthedUser } from "./authedUser"
+import { showLoading, hideLoading } from "react-redux-loading";
 
 function selectAuthedId () {
     const users = ["sarah_edo", "tylermcginnis", "dan_abramov"];
@@ -12,11 +13,13 @@ const AUTHED_ID = selectAuthedId();
 
 export function handleInitialData () {
     return dispatch => {
+        dispatch(showLoading());
         return getInitialData()
             .then(({ users, tweets }) => {
                 dispatch(receiveUsers(users));
                 dispatch(receiveTweets(tweets));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             });
     };
 }
